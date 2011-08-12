73ea1015fd3797ffaed895a274b2b5479004f767
rhchen <ranhow.chen@gmail.com>
2011-08-13 00:07:19 +0800

    Add ApplicationActionBarAdvisor, implementa reuquired menu and coolbars
    	modified:   rcp/plugins/net.izhtw.rcp.explorer.master/META-INF/MANIFEST.MF
    	modified:   rcp/plugins/net.izhtw.rcp.explorer.master/explorer.product
    	modified:   rcp/plugins/net.izhtw.rcp.explorer.master/plugin.xml
    	new file:   rcp/plugins/net.izhtw.rcp.explorer.master/src/net/izhtw/rcp/explorer/master/ApplicationActionBarAdvisor.java
    	modified:   rcp/plugins/net.izhtw.rcp.explorer.master/src/net/izhtw/rcp/explorer/master/ApplicationWorkbenchWindowAdvisor.java

--- rcp/plugins/net.izhtw.rcp.explorer.master/src/net/izhtw/rcp/explorer/master/ApplicationWorkbenchWindowAdvisor.java
+++ rcp/plugins/net.izhtw.rcp.explorer.master/src/net/izhtw/rcp/explorer/master/ApplicationWorkbenchWindowAdvisor.java
@@ -1,6 +1,8 @@
 package net.izhtw.rcp.explorer.master;
 
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.ui.application.ActionBarAdvisor;
+import org.eclipse.ui.application.IActionBarConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 
@@ -15,6 +17,12 @@ public class ApplicationWorkbenchWindowAdvisor extends WorkbenchWindowAdvisor {
 		IWorkbenchWindowConfigurer configurer = getWindowConfigurer();
 		configurer.setInitialSize(new Point(800, 600));
 		configurer.setShowProgressIndicator(true);
+	}
+
+	@Override
+	public ActionBarAdvisor createActionBarAdvisor(
+			IActionBarConfigurer configurer) {
+		return new ApplicationActionBarAdvisor(configurer);
 	}
 
 }
